93c6290b5e9ba38a816fddd334ad65d3df6f8c56
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-03-16 14:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ function palindrome(str){
 // 	}
 // 	return verdict;
 // }
- str.toLowerCase();
+ const string = str.toLowerCase();
  const len = str.length;
 
     // loop through half of the string
     for (let i = 0; i < len / 2; i++) {
 
         // check if first and last string are same
-        if (str[i] !== str[len - 1 - i]) {
+        if (string[i] !== string[len - 1 - i]) {
             return false;
         }
     }
